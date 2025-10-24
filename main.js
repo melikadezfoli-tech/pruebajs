ebc27e313fbd8d360f2a499c8c6c98a0a75f9473
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-10-24 10:10:19 +0200

    commit2

---
+++ main.js
@@ -0,0 +1,35 @@
+/*Ejercicio 1
+Declara una función que acepte una palabra como parámetro y 
+la devuelva con todos sus caracteres en mayúsculas y separados 
+por espacios. Ejemplo: Para "Ana" devolverá "A N A".*/
+
+/*let palabra="Ana";
+transformar= console.log(palabra.toUppercase());*/
+
+/*Ejercicio 2
+Declara una función que acepte un nombre como parámetro y devuelva 
+el número de palabras que tiene el nombre. Ejemplo: Para "Antonio Alberto Jesús" 
+devolverá 3.*/
+
+/*var nombre="Antonio Alberto Jesús";
+totalparam = console.log(nombre.length);*/
+
+/*Ejercicio 3:
+Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase.*/
+
+/*1.Inicio
+2.Leer frasse
+3.Verificar que es tipo string
+4.Inicializacion variables en 1
+5.bucle para recorrer la frase
+6.si en cada iteración hay una letra sea igual a letra suma*/
+
+let frase= "Esto es una frase";
+let letra= substring (frase);
+
+/*for (let i; i<=frase.length; i++) {
+    if 
+}*/
+
+
+
